bd0ac7063c2966b3a6dd2fba8365fe9823247b18
manuelfvalless <manuelfvalless@gmail.com>
2018-12-03 13:02:48 -0300

    Actualización

--- WebTurbus2/src/test/java/com/turbus/webturbus2/W2E1_Caso013.java
+++ WebTurbus2/src/test/java/com/turbus/webturbus2/W2E1_Caso013.java
@@ -2,12 +2,15 @@ package com.turbus.webturbus2;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -27,21 +30,24 @@ public class W2E1_Caso013 {
                 "src/test/java/driver/phantomjs.exe");
         driver = new PhantomJSDriver(capabilities);*/
         System.setProperty("webdriver.chrome.driver", "src/test/java/driver/chromedriver.exe");
+        driver = new ChromeDriver();
         baseUrl = "http://172.20.4.233:8080/turbuscl/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void testW2E1_Caso013() throws Exception {
-        getFoto(driver);
         driver.get(baseUrl);
+        getFoto(driver);
         Thread.sleep(8000);
         driver.findElement(By.id("destino")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ingrese ciudad de destino'])[2]/following::input[1]")).clear();
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ingrese ciudad de destino'])[2]/following::input[1]")).sendKeys("Temuco");
+        getFoto(driver);
         Thread.sleep(1000);
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ciudad de destino'])[1]/following::p[1]")).click();
+        getFoto(driver);
         Thread.sleep(8000);
         try {
             assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ciudad de destino'])[1]/following::p[1]")).getText(), "Ciudades sugeridas");
@@ -96,8 +102,14 @@ public class W2E1_Caso013 {
 
     private void getFoto(WebDriver webDriver) {
         try {
+            File path = new File("D:\\Git\\turbus_web2\\Evidencia\\W2E1_Caso013");
+            path.mkdir();
+            Date d = new Date();
+            SimpleDateFormat sd = new SimpleDateFormat("dd_MM_yy_HH_mm_ss_a");
+            String timestamp = sd.format(d);
+            String imgname = path + "\\" + timestamp + ".png";
             File screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(screenshot, new File( ".png"));     
+            FileUtils.copyFile(screenshot, new File(imgname));
         } catch (IOException ex) {
             System.out.println("Error al capturar secuencia: " + ex);
         }
