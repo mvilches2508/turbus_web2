f543487308097ac81d3d34dc3134a1872cc37f7a
manuelfvalless <manuelfvalless@gmail.com>
2018-12-10 13:50:43 -0300

    Actualización

---
+++ WebTurbus2/src/test/java/com/turbus/webturbus2/W2E1_Caso001.java
@@ -0,0 +1,21 @@
+package com.turbus.webturbus2;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class W2E1_Caso001 {
+    
+}
